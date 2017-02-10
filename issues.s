e77c2b0de1bae374190fc15859d88beaf0d56278
jieqwu <jieqian_wu@126.com>
2017-02-10 10:31:38 +0800

    Implement simple classes:  base - command, subclass - RunInstance.

---
+++ issues.s
@@ -0,0 +1,25 @@
+Change Request 1
+>&---------bad request------
+count 1
+signature_method HmacSHA256
+zone 'pek3a'
+instance_type small_b
+signature_version 1
+signature VUy8Ek9RFyY51jSUQafd2tcjJJyMe3XgZIJAgepExVM%3D
+instance_name test
+image_id centos64x86a
+vnets.1 vnet-0
+version 1
+access_key_id 'YFHFNVOBCPSMOAVFKQQU'
+action RunInstances
+url = count=1&signature_method=HmacSHA256&zone=%27pek3a%27&instance_type=small_b&signature_version=1&signature=VUy8Ek9RFyY51jSUQafd2tcjJJyMe3XgZIJAgepExVM%253D&instance_name=test&image_id=centos64x86a&vnets.1=vnet-0&version=1&access_key_id=%27YFHFNVOBCPSMOAVFKQQU%27&action=RunInstances
+200
+
+<html>
+<head><title>400 Bad Request</title></head>
+<body bgcolor="white">
+<center><h1>400 Bad Request</h1></center>
+<hr><center>nginx/1.1.19</center>
+</body>
+</html>
+>&-------------
